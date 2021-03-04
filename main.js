7662c2a93781df312c398645412eca3a50587fdc
hj.seong <hj.seong@lgresearch.ai>
2021-03-04 15:35:19 +0900

    set logic - 함수 적용하여 개선

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 (() => {
   const stepElems = document.querySelectorAll(".step");
   const graphicElems = document.querySelectorAll(".graphic-item");
-  let currentItem; // 현재 활성화된 .graphic-item을 지정
+  let currentItem = graphicElems[0]; // 현재 활성화된 .graphic-item을 지정
 
   for (let i = 0; i < stepElems.length; i++) {
     // stepElems[i].setAttribute("data-index", i);
     stepElems[i].dataset.index = i;
     graphicElems[i].dataset.index = i;
+  }
+
+  function activate() {
+    currentItem.classList.add("visible");
+  }
+
+  function inactivate() {
+    currentItem.classList.remove("visible");
   }
 
   window.addEventListener("scroll", () => {
@@ -20,12 +28,12 @@
         boundingRect.top > window.innerHeight * 0.1 &&
         boundingRect.top < window.innerHeight * 0.8
       ) {
-        if (currentItem) {
-          currentItem.classList.remove("visible");
-        }
+        inactivate();
         currentItem = graphicElems[step.dataset.index];
-        currentItem.classList.add("visible");
+        activate();
       }
     }
   });
+
+  activate();
 })();
