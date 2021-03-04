bb2086c3023d576c943f52887305031a2bfac8eb
hj.seong <hj.seong@lgresearch.ai>
2021-03-04 16:41:03 +0900

    for문 성능 개선

--- main.js
+++ main.js
@@ -2,8 +2,14 @@
   const stepElems = document.querySelectorAll(".step");
   const graphicElems = document.querySelectorAll(".graphic-item");
   let currentItem = graphicElems[0]; // 현재 활성화된 .graphic-item을 지정
+  let ioIndex;
+
+  const io = new IntersectionObserver((entries, observer) => {
+    ioIndex = entries[0].target.dataset.index * 1;
+  });
 
   for (let i = 0; i < stepElems.length; i++) {
+    io.observe(stepElems[i]);
     // stepElems[i].setAttribute("data-index", i);
     stepElems[i].dataset.index = i;
     graphicElems[i].dataset.index = i;
@@ -21,9 +27,11 @@
     let step;
     let boundingRect;
 
-    for (let i = 0; i < stepElems.length; i++) {
+    for (let i = ioIndex - 1; i < ioIndex + 2; i++) {
       step = stepElems[i];
+      if (!step) continue;
       boundingRect = step.getBoundingClientRect();
+
       if (
         boundingRect.top > window.innerHeight * 0.1 &&
         boundingRect.top < window.innerHeight * 0.8
